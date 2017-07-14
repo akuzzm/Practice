31dabdb4a5048c725786de34f36550ce801a54f9
akuzzm <akuzzm@gmail.com>
2017-07-14 17:18:28 +0000

    Add Hello and age

--- Maximum.py
+++ Maximum.py
@@ -1,3 +1,5 @@
+def hello(name):
+    print "Hello {} I am glad to see you!!!".format(name)
 
 def maximum(some_list):
     """Get maximum value from list"""
@@ -6,3 +8,9 @@ def maximum(some_list):
 def count(some_list):
     """Count elements in list"""
     returt len(some_list)
+
+def check_age(age):
+    if age < 18:
+       return False
+    else:
+       return True
